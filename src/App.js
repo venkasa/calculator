0dbc53275168c18882ef620699fd392b7e50708f
VenkateshKrishna <venkateshkrishna997@gmail.com>
2022-11-24 19:05:46 +0530

    adds css and ui for the calculator

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">234,458 +</div>
+        <div className="current-operand">874,987</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
